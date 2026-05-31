a7b8f94ff66dde7411bb39223b88bb7150434abb
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-31 16:10:46 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+HF_TOKEN = os.environ.get("HF_TOKEN", "")
+DATA_REPO = "P2SAMAPA/fi-etf-macro-signal-master-data"
+OUTPUT_REPO = "P2SAMAPA/p2-etf-moran-process-results"
+
+WINDOWS = [63, 252, 504, 1008, 2016, 4032, 4536]
+
+UNIVERSES = {
+    "FI_COMMODITIES": ["TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV"],
+    "EQUITY_SECTORS": [
+        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
+        "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "IWM", "IWD", "IWO"
+    ],
+    "COMBINED": [
+        "TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV",
+        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
+        "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "IWM", "IWD", "IWO"
+    ]
+}
+
+# Moran process parameters
+POPULATION_SIZE = 1000          # total number of "individuals" (virtual)
+SIMULATION_STEPS = 5000         # steps to reach near-stationary
+MUTATION_RATE = 0.0             # no mutation (pure selection)
+TOP_N = 3
